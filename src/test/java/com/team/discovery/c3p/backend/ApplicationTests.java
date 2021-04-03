f6dfc9a6a8b81f8ddaeb00272f6ea670ce325d2a
Diego Borrero <diego.borrero@payulatam.com>
2021-04-03 12:47:37 -0500

    First Commit

---
+++ src/test/java/com/team/discovery/c3p/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.discovery.c3p.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
